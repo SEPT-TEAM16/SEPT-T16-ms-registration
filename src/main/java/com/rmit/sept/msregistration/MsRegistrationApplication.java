882f277cc57ae6492ee535f814d85f65ba647917
Daniel <s3786266@student.rmit.edu.au>
2022-08-24 18:46:55 +1000

    Created gradle project structure

---
+++ src/main/java/com/rmit/sept/msregistration/MsRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.msregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsRegistrationApplication.class, args);
+    }
+
+}
