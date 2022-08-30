61d81b0aca003318b018e6e40bbf057c0de98238
Wyatt Jenkins (s3770872) <s3770872@student.rmit.edu.au>
2022-08-30 21:50:26 +1000

    Added logging in registration service

--- src/main/java/com/rmit/sept/msregistration/service/RegistrationService.java
+++ src/main/java/com/rmit/sept/msregistration/service/RegistrationService.java
@@ -2,6 +2,8 @@ package com.rmit.sept.msregistration.service;
 
 import com.rmit.sept.msregistration.model.User;
 import com.rmit.sept.msregistration.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +12,19 @@ import java.util.Optional;
 @Service
 public class RegistrationService {
 
+    Logger log = LoggerFactory.getLogger(this.getClass().getName());
+
     @Autowired
     private UserRepository userRepository;
 
     public User saveNewUserDetails(User userInfo) {
-
+        log.info("Saving new user with userInfo={}", userInfo.toString());
         return userRepository.save(userInfo);
 
     }
 
-    public Optional<User> getExistingUserDetails(String userId) {
-
+    public Optional<User> getExistingUserDetails(Integer userId) {
+        log.info("Fetching details for user with userId={}", userId);
         return userRepository.findById(userId);
     }
 }
