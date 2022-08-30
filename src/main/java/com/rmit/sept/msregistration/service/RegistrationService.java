9a857ef021f4374ffe335c52ed3a6a139d66ac94
Daniel Gao <s3786266@student.rmit.edu.au>
2022-08-30 17:56:27 +0800

    Created methods for service layer

--- src/main/java/com/rmit/sept/msregistration/service/RegistrationService.java
+++ src/main/java/com/rmit/sept/msregistration/service/RegistrationService.java
@@ -1,4 +1,26 @@
 package com.rmit.sept.msregistration.service;
 
+import com.rmit.sept.msregistration.model.User;
+import com.rmit.sept.msregistration.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class RegistrationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveNewUserDetails(User userInfo) {
+
+        return userRepository.save(userInfo);
+
+    }
+
+    public Optional<User> getExistingUserDetails(String userId) {
+
+        return userRepository.findById(userId);
+    }
 }
