1e85347bcdfe389788b90c1fa402fea2b2dce034
Daniel <s3786266@student.rmit.edu.au>
2022-09-20 20:17:56 +1000

    added some comments to explain the test cases

--- src/test/java/com/rmit/sept/msregistration/unit/exception/ExceptionHandlerTest.java
+++ src/test/java/com/rmit/sept/msregistration/unit/exception/ExceptionHandlerTest.java
@@ -34,15 +34,22 @@ public class ExceptionHandlerTest {
 
     @Test
     public void handleInvalidRequest_MissingParameterException() {
-
+        // given I have mocked the exception class behaviour
         RollbackException exception = mock(RollbackException.class);
 
+        // when the exception is called in the global exception handler
+        // I want to store the response in the error model
         ResponseEntity<ErrorModel> errorResponse = handler.handleMissingParameter(exception, request);
+
+        // then ensure the error response is returning the correct fields declared in the global
+        // exception handler
         assertAll("errorResponse",
                 () -> {
+            // it is asserting that the expected status should match the actual status of "BAD REQUEST"
             assertThat(errorResponse.getStatusCode(), is(HttpStatus.BAD_REQUEST));
             assertNotNull(errorResponse.getBody());
             ErrorModel body = errorResponse.getBody();
+            // it is asserting that the expected error details should match the custom error message
             assertAll("body",
                     () -> assertThat(body.getErrorDetails(), is("Request has failed due to invalid method argument, please check your json response body"))
 
@@ -72,15 +79,22 @@ public class ExceptionHandlerTest {
 
     @Test
     public void handleInvalidRequest_UserId() {
-
+        // given I have mocked the exception class behaviour
         UserIdException exception = mock(UserIdException.class);
 
+        // when the exception is called in the global exception handler
+        // I want to store the response in the error model
         ResponseEntity<ErrorModel> errorResponse = handler.handleInvalidUserId(exception, request);
+
+        // then ensure the error response is returning the correct fields declared in the global
+        // exception handler
         assertAll("errorResponse",
                 () -> {
+                    // it is asserting that the expected HTTP status should match the actual status of "NOT FOUND"
                     assertThat(errorResponse.getStatusCode(), is(HttpStatus.NOT_FOUND));
                     assertNotNull(errorResponse.getBody());
                     ErrorModel body = errorResponse.getBody();
+                    // it is asserting that the expected error details should match the custom error message
                     assertAll("body",
                             () -> assertThat(body.getErrorDetails(), is("Request has failed due to invalid request, please check your json input"))
 
