b423ab9bc4e02abdd4afd2eb19d48f3bc457e424
Daniel <s3786266@student.rmit.edu.au>
2022-09-26 23:50:19 +1000

    created logic to update and save new user details

--- src/main/java/com/rmit/sept/msregistration/service/RegistrationService.java
+++ src/main/java/com/rmit/sept/msregistration/service/RegistrationService.java
@@ -1,7 +1,10 @@
 package com.rmit.sept.msregistration.service;
 
+import com.rmit.sept.msregistration.constants.AppRole;
 import com.rmit.sept.msregistration.exception.UserIdException;
+import com.rmit.sept.msregistration.model.MedicalRecord;
 import com.rmit.sept.msregistration.model.User;
+import com.rmit.sept.msregistration.repository.MedicalRepository;
 import com.rmit.sept.msregistration.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +21,11 @@ public class RegistrationService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private MedicalRepository medicalRepository;
+
     public User saveNewUserDetails(User userInfo) {
+
         log.info("Saving new user with userInfo={}", userInfo.toString());
         return userRepository.save(userInfo);
 
@@ -33,5 +40,39 @@ public class RegistrationService {
             throw new UserIdException("user Id is not found in the database", userId);
         }
         return userDetails;
+    }
+
+    public User updateExistingUserDetails(User newUserDetails, Integer userId) {
+        log.info("Fetching details for update existing user method with userId={}", userId);
+
+        Optional<User> userInfo = userRepository.findById(userId);
+
+        if (userInfo.isPresent()) {
+            userInfo.get().setFirstName(newUserDetails.getFirstName());
+            userInfo.get().setLastName(newUserDetails.getLastName());
+            userInfo.get().setDoB(newUserDetails.getDoB());
+            userInfo.get().setEmail(newUserDetails.getEmail());
+            userInfo.get().setAddress(newUserDetails.getAddress());
+            userInfo.get().setPassword(newUserDetails.getPassword());
+            userInfo.get().setMobileNumber(userInfo.get().getMobileNumber());
+
+            log.info("Fetching old user details={} from database", userInfo);
+            log.info("Updating new user details={}", newUserDetails);
+
+            return userRepository.save(userInfo.get());
+        }
+        throw new UserIdException("user Id is not found in the database", userId);
+    }
+
+    public void deleteExistingUserDetails(Integer userId) {
+       userRepository.deleteById(userId);
+    }
+    public MedicalRecord saveNewMedicalInfo(MedicalRecord medicalInfo, Integer userId) {
+
+        Optional<User> userDetails = userRepository.findById(userId);
+        if(!userDetails.isPresent()){
+            throw new UserIdException("user Id is not found in the database", userId);
+        }
+        return medicalRepository.save(medicalInfo);
     }
 }
