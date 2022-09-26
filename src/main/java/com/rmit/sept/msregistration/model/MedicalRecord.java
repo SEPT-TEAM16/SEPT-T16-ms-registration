a1b6764c040e0d9fc2d65c0b521d3698e9bb0907
Daniel <s3786266@student.rmit.edu.au>
2022-09-26 19:00:35 +1000

    created medical record entity

---
+++ src/main/java/com/rmit/sept/msregistration/model/MedicalRecord.java
@@ -0,0 +1,37 @@
+package com.rmit.sept.msregistration.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "medical-record")
+public class MedicalRecord {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "medical_id")
+    private Integer medicalId;
+
+    @Column(name = "medicare_number")
+    private String medicareNo;
+
+    @Column(name = "height")
+    private String height;
+
+    @Column(name = "weight")
+    private String weight;
+
+    @Column(name = "medical_history")
+    private String medicalHistory;
+
+    @Column(name = "gender")
+    private String gender;
+}
