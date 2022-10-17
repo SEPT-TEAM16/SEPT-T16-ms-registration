9861687836e470962ffaf66e86432495e0bace26
Daniel <s3786266@student.rmit.edu.au>
2022-10-17 17:51:13 +1100

    refracted and tidied up the code

--- src/main/java/com/rmit/sept/msregistration/repository/UserRepository.java
+++ src/main/java/com/rmit/sept/msregistration/repository/UserRepository.java
@@ -1,9 +1,6 @@
 package com.rmit.sept.msregistration.repository;
 
-
-import com.rmit.sept.msregistration.model.Patient;
 import com.rmit.sept.msregistration.model.User;
-import org.hibernate.annotations.Subselect;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
